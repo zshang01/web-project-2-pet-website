52f40462f34f0cb2841556b2e3149c2fcac23743
beefman92 <bob_zhao@163.com>
2019-02-22 08:48:46 -0800

    complete part of navigation bar

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,11 +1,89 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
+import {Redirect} from "react-router-dom";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import Cookies from "universal-cookie";
+import Button from "react-bootstrap/Button";
 
 class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false,
+			username: "",
+			password: "",
+		};
+	}
+
+	handleOnChange(event) {
+		const name = event.target.name;
+		const value = event.target.value;
+
+		this.setState({
+			[name]: value,
+		});
+	}
+
+	handleOnSubmit(event) {
+		event.preventDefault();
+		// TODO: handle submission info
+	}
+
+	componentDidMount() {
+		const cookies = new Cookies();
+		const token = cookies.get("pwLoggedIn");
+		if (token !== undefined && token !== null && token !== "") {
+			this.setState({
+				loggedIn: true,
+			});
+		} else {
+			this.setState({
+				loggedIn: false,
+			});
+		}
+	}
+
 	render() {
-		return (
-			<h1>Login</h1>
-		);
+		if (this.state.loggedIn) {
+			return (<Redirect to={"/"} />);
+		} else {
+			return (
+				<Container>
+					<Row>
+						<Col className={"my-4"} lg={"12"}>
+							<h1>Sign In</h1>
+						</Col>
+					</Row>
+					<Form onSubmit={(e) => this.handleOnSubmit(e)}>
+						<Form.Row>
+							<Form.Group controlId={"formUsername"}>
+								<Form.Label>Username: </Form.Label>
+								<Form.Control
+									as={"input"}
+									type={"text"}
+									name={"username"}
+									value={this.state.username}
+									onChange={(e) => this.handleOnChange(e)} />
+							</Form.Group>
+						</Form.Row>
+						<Form.Row>
+							<Form.Group controlId={"formPassword"}>
+								<Form.Label>Password: </Form.Label>
+								<Form.Control
+									as={"input"}
+									type={"password"}
+									name={"password"}
+									value={this.state.password}
+									onChange={(e) => this.handleOnChange(e)} />
+							</Form.Group>
+						</Form.Row>
+						<Form.Row>
+							<Button variant={"primary"} type={"submit"}>Submit</Button>
+						</Form.Row>
+					</Form>
+				</Container>
+			);
+		}
 	}
 }
 
--- front/src/pages/NavigationBar.js
+++ front/src/pages/NavigationBar.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, ButtonToolbar, Button } from "react-bootstrap";
+import Cookies from "universal-cookie";
+import "../css/navigation-bar.css";
 
 class NavigationBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false,
+		};
+	}
+
+	componentDidMount() {
+		const cookies = new Cookies();
+		const token = cookies.get("pwLoggedIn");
+		if (token !== undefined && token !== null && token !== "") {
+			this.setState({
+				loggedIn: true,
+			});
+		} else {
+			this.setState({
+				loggedIn: false,
+			});
+		}
+	}
+
 	render() {
+		let rightCornerContent = null;
+		if (this.state.loggedIn) {
+			rightCornerContent = (
+				<a href="/user-profile"><img id={"rightCornerIcon"} src="images/avatar.svg" alt="" /></a>
+			);
+		} else {
+			rightCornerContent = (
+				<ButtonToolbar>
+					<Button className={"mx-1"} variant={"primary"} href={"/register"}>Register</Button>
+					<Button className={"mx-1"} variant={"success"} href={"/login"}>Sign in</Button>
+				</ButtonToolbar>
+			);
+		}
 		return (
 			<Navbar	bg="light" expand="lg">
 				<Navbar.Brand href="/">Pet Website</Navbar.Brand>
@@ -11,9 +47,8 @@ class NavigationBar extends React.Component {
 					<Nav className="mr-auto">
 						<Nav.Link href="/">Homepage</Nav.Link>
 						<Nav.Link href="/recommendation">Recommendation</Nav.Link>
-						<Nav.Link href="/register">Register</Nav.Link>
-						<Nav.Link href="/user-profile">User</Nav.Link>
 					</Nav>
+					{rightCornerContent}
 				</Navbar.Collapse>
 			</Navbar>
 		);
